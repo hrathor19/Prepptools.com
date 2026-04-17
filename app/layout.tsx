45dbe78b932f4bfd0b475e1c47710c17b05d5769
Himanshu Rathore <hrathor19@gmail.com>
2026-04-17 13:27:12 +0530

    add: Microsoft Clarity tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,12 @@ export default function RootLayout({
             __html: `try{var t=localStorage.getItem('theme');if(t==='dark'){document.documentElement.classList.add('dark')}}catch(e){}`,
           }}
         />
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","wd07x3k632");`,
+          }}
+        />
         {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-GC16LZSHH1" />
         <script
